80bb49c5a227b70881ba396fb72ff4d553525cac
Diogo Alves <alvessilva524@gmail.com>
2024-01-12 12:46:52 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { NgxPanZoomModule } from 'ngx-panzoom';
+import { OrgComponent } from './org/org.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrgComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OrganizationChartModule,
+    NgxPanZoomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
